67cb5c15e27947cd2fab72b7bb5c2e33b1b0a568
abetomasa <abetomasa@gmail.com>
2018-04-18 08:57:03 +0000

    kadaiL12-9

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = current_user.tasks if logged_in?
@@ -14,11 +16,12 @@ class TasksController < ApplicationController
 
   def create
      @task = current_user.tasks.build(task_params)
-
+     
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -40,7 +43,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -48,9 +51,18 @@ class TasksController < ApplicationController
   def set_message
     @task = Task.find(params[:id])
   end
+  
+  
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
