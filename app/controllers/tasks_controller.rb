11cc2c3c37bb61c054f926400c4cffdc61253a19
abetomasa <abetomasa@gmail.com>
2018-04-18 07:39:17 +0000

    kadaiL12-9

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks if logged_in?
   end
 
   def show
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,6 @@
 Rails.application.routes.draw do
-  
-  
-  get 'users/index'
 
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  root to: 'users#index'
+  root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
